a4dc051560e67fa19d2d501801c9c220a399debe
SandeepKumar <sandeepkusolanki@gamil.com>
2025-03-30 15:11:18 +0530

    greet

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [username , setUsername] = useState("")
+
+  function handleChange(e){
+    setUsername(e.target.value)
+  }
   return (
-    <div>
+
+      <div>
         {/* Do not remove the main div */}
+        <h1>Enter your name</h1>
+        <form>
+          <input onChange={handleChange}
+
+          />
+          <h3>Hello {username}</h3>
+          
+        </form>
     </div>
   )
 }
